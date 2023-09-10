78b229709622a04d9e7a94683c26b00d96dc5c1b
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-11 01:56:54 +0530

    Update : Cors Origin

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://stackoverflow-clone-backend-vrer.onrender.com',
 }));
 
 app.use('/api/users', userRoutes);
