dfcfa927e51202d44b5f56325bb8f478670f30a4
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-11 01:47:21 +0530

    Fix : Nodemon error

--- index.ts
+++ index.ts
@@ -31,3 +31,12 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
+});
