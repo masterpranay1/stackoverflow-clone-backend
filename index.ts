ba2ad76308203d48f42f763cc32e8c1abda30f3c
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-11 09:54:35 +0530

    Fix : Cors Error

--- index.ts
+++ index.ts
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
-    'https://stackoverflow-clone-backend-vrer.onrender.com',
-    'http://localhost:5173',
+    'https://stackoverflow-clone-frontend.onrender.com',
   ],
 }));
 
